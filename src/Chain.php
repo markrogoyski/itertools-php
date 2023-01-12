dd9049a803a79d55598139ee8fb3626f5636b253
Smoren <ofigate@gmail.com>
2023-01-12 12:20:30 +0300

    Namespace `Chain`: all methods added, not tested yet.

--- src/Chain.php
+++ src/Chain.php
@@ -7,8 +7,6 @@ namespace IterTools;
 use IterTools\Util\IteratorFactory;
 
 /**
- * @template mixed
- * @template mixed
  * @implements \IteratorAggregate<mixed>
  */
 class Chain implements \IteratorAggregate
@@ -104,12 +102,235 @@ class Chain implements \IteratorAggregate
     }
 
     /**
+     * @param iterable<mixed> ...$iterables
+     *
+     * @return $this
+     */
+    public function chainWith(iterable ...$iterables): self
+    {
+        $this->iterable = Multi::chain($this->iterable, ...$iterables);
+        return $this;
+    }
+
+    /**
+     * @param iterable<mixed> ...$iterables
+     *
+     * @return $this
+     */
+    public function zipWith(iterable ...$iterables): self
+    {
+        $this->iterable = Multi::zip($this->iterable, ...$iterables);
+        return $this;
+    }
+
+    /**
+     * @param iterable<mixed> ...$iterables
+     *
+     * @return $this
+     */
+    public function zipLongestWith(iterable ...$iterables): self
+    {
+        $this->iterable = Multi::zipLongest($this->iterable, ...$iterables);
+        return $this;
+    }
+
+    /**
+     * @param iterable<mixed> ...$iterables
+     *
+     * @return $this
+     */
+    public function zipEqualWith(iterable ...$iterables): self
+    {
+        $this->iterable = Multi::zipEqual($this->iterable, ...$iterables);
+        return $this;
+    }
+
+    /**
      * @return $this
      */
     public function infiniteCycle(): self
     {
         $this->iterable = Infinite::cycle($this->iterable);
         return $this;
+    }
+
+    /**
+     * @param int|float|null $initialValue
+     *
+     * @return $this
+     */
+    public function runningAverage($initialValue = null): self
+    {
+        /** @var iterable<int|float> $iterable */
+        $iterable = $this->iterable;
+        $this->iterable = Math::runningAverage($iterable, $initialValue);
+        return $this;
+    }
+
+    /**
+     * @param int|float|null $initialValue
+     *
+     * @return $this
+     */
+    public function runningDifference($initialValue = null): self
+    {
+        /** @var iterable<int|float> $iterable */
+        $iterable = $this->iterable;
+        $this->iterable = Math::runningDifference($iterable, $initialValue);
+        return $this;
+    }
+
+    /**
+     * @param int|float|null $initialValue
+     *
+     * @return $this
+     */
+    public function runningMax($initialValue = null): self
+    {
+        /** @var iterable<int|float> $iterable */
+        $iterable = $this->iterable;
+        $this->iterable = Math::runningMax($iterable, $initialValue);
+        return $this;
+    }
+
+    /**
+     * @param int|float|null $initialValue
+     *
+     * @return $this
+     */
+    public function runningMin($initialValue = null): self
+    {
+        /** @var iterable<int|float> $iterable */
+        $iterable = $this->iterable;
+        $this->iterable = Math::runningMin($iterable, $initialValue);
+        return $this;
+    }
+
+    /**
+     * @param int|float|null $initialValue
+     *
+     * @return $this
+     */
+    public function runningProduct($initialValue = null): self
+    {
+        /** @var iterable<int|float> $iterable */
+        $iterable = $this->iterable;
+        $this->iterable = Math::runningProduct($iterable, $initialValue);
+        return $this;
+    }
+
+    /**
+     * @param int|float|null $initialValue
+     *
+     * @return $this
+     */
+    public function runningTotal($initialValue = null): self
+    {
+        /** @var iterable<int|float> $iterable */
+        $iterable = $this->iterable;
+        $this->iterable = Math::runningTotal($iterable, $initialValue);
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSorted(): bool
+    {
+        return Summary::isSorted($this->iterable);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isReversed(): bool
+    {
+        return Summary::isReversed($this->iterable);
+    }
+
+    /**
+     * @param iterable<mixed> ...$iterables
+     *
+     * @return bool
+     */
+    public function sameWith(iterable ...$iterables): bool
+    {
+        return Summary::same($this->iterable, ...$iterables);
+    }
+
+    /**
+     * @param iterable<mixed> ...$iterables
+     *
+     * @return bool
+     */
+    public function sameCountWith(iterable ...$iterables): bool
+    {
+        return Summary::sameCount($this->iterable, ...$iterables);
+    }
+
+    /**
+     * @return int|float|null
+     */
+    public function toAverage()
+    {
+        /** @var iterable<numeric> $iterable */
+        $iterable = $this->iterable;
+        return Reduce::toAverage($iterable);
+    }
+
+    /**
+     * @return int
+     */
+    public function toCount(): int
+    {
+        return Reduce::toCount($this->iterable);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function toMax()
+    {
+        return Reduce::toMax($this->iterable);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function toMin()
+    {
+        return Reduce::toMin($this->iterable);
+    }
+
+    /**
+     * @return int|float|null
+     */
+    public function toProduct()
+    {
+        /** @var iterable<numeric> $iterable */
+        $iterable = $this->iterable;
+        return Reduce::toProduct($iterable);
+    }
+
+    /**
+     * @return numeric
+     */
+    public function toSum()
+    {
+        /** @var iterable<numeric> $iterable */
+        $iterable = $this->iterable;
+        return Reduce::toSum($iterable);
+    }
+
+    /**
+     * @param callable $reducer
+     * @param mixed $initialValue
+     *
+     * @return mixed
+     */
+    public function toValue(callable $reducer, $initialValue = null)
+    {
+        return Reduce::toValue($this->iterable, $reducer, $initialValue);
     }
 
     /**
