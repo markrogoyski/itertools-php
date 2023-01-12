3549b2727da66c1f646f253214580053d0e0d8c1
Smoren <ofigate@gmail.com>
2023-01-12 21:22:10 +0300

    `Chain`: tests added.

--- tests/Chain/ReduceTest.php
+++ tests/Chain/ReduceTest.php
@@ -6,142 +6,16 @@ namespace IterTools\Tests\Chain;
 
 use IterTools\Chain;
 
-class SingleAndReduceTest extends \PHPUnit\Framework\TestCase
+class ReduceTest extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * @param array $input
-     * @param callable $chainMaker
-     * @param array $expected
-     * @return void
-     * @dataProvider dataProviderForArray
-     */
-    public function testArray(array $input, callable $chainMaker, array $expected): void
-    {
-        // Given
-        $result = [];
-
-        // When
-        $chain = $chainMaker($input);
-        foreach ($chain as $value) {
-            $result[] = $value;
-        }
-
-        // Then
-        $this->assertSame($expected, $result);
-    }
-
-    public function dataProviderForArray(): array
-    {
-        return [
-            [
-                [],
-                static function (array $iterable) {
-                    return Chain::create($iterable);
-                },
-                [],
-            ],
-            [
-                [1, -1, 2, -2, 3, -3],
-                static function (array $iterable) {
-                    return Chain::create($iterable);
-                },
-                [1, -1, 2, -2, 3, -3],
-            ],
-            [
-                [],
-                static function (array $iterable) {
-                    return Chain::create($iterable)
-                        ->filterTrue(static function ($value) {
-                            return $value > 0;
-                        });
-                },
-                [],
-            ],
-            [
-                [1, -1, 2, -2, 3, -3],
-                static function (array $iterable) {
-                    return Chain::create($iterable)
-                        ->filterTrue(static function ($value) {
-                            return $value > 0;
-                        });
-                },
-                [1, 2, 3],
-            ],
-            [
-                [],
-                static function (array $iterable) {
-                    return Chain::create($iterable)
-                        ->filterFalse(static function ($value) {
-                            return $value > 0;
-                        })
-                        ->pairwise();
-                },
-                [],
-            ],
-            [
-                [1, -1, 2, -2, 3, -3],
-                static function (array $iterable) {
-                    return Chain::create($iterable)
-                        ->filterFalse(static function ($value) {
-                            return $value > 0;
-                        });
-                },
-                [-1, -2, -3],
-            ],
-            [
-                [],
-                static function (array $iterable) {
-                    return Chain::create($iterable)
-                        ->filterTrue(static function ($value) {
-                            return $value > 0;
-                        })
-                        ->pairwise();
-                },
-                [],
-            ],
-            [
-                [1, -1, 2, -2, 3, -3],
-                static function (array $iterable) {
-                    return Chain::create($iterable)
-                        ->filterTrue(static function ($value) {
-                            return $value > 0;
-                        })
-                        ->pairwise();
-                },
-                [[1, 2], [2, 3]],
-            ],
-            [
-                [],
-                static function (array $iterable) {
-                    return Chain::create($iterable)
-                        ->filterFalse(static function ($value) {
-                            return $value > 0;
-                        })
-                        ->pairwise();
-                },
-                [],
-            ],
-            [
-                [1, -1, 2, -2, 3, -3],
-                static function (array $iterable) {
-                    return Chain::create($iterable)
-                        ->filterFalse(static function ($value) {
-                            return $value > 0;
-                        })
-                        ->pairwise();
-                },
-                [[-1, -2], [-2, -3]],
-            ],
-        ];
-    }
     /**
      * @param array $input
      * @param callable $reducer
      * @param mixed $expected
      * @return void
-     * @dataProvider dataProviderForArrayReduce
+     * @dataProvider dataProviderForArray
      */
-    public function testArrayReduce(array $input, callable $reducer, $expected): void
+    public function testArray(array $input, callable $reducer, $expected): void
     {
         // Given
         // When
@@ -151,7 +25,7 @@ class SingleAndReduceTest extends \PHPUnit\Framework\TestCase
         $this->assertSame($expected, $result);
     }
 
-    public function dataProviderForArrayReduce(): array
+    public function dataProviderForArray(): array
     {
         return [
             [
@@ -259,6 +133,64 @@ class SingleAndReduceTest extends \PHPUnit\Framework\TestCase
                         ->toCount();
                 },
                 2,
+            ],
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable)->toAverage();
+                },
+                null,
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable)->toAverage();
+                },
+                0,
+            ],
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterTrue(static function ($value) {
+                            return $value > 0;
+                        })
+                        ->toAverage();
+                },
+                null,
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterTrue(static function ($value) {
+                            return $value > 0;
+                        })
+                        ->toAverage();
+                },
+                2,
+            ],
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterFalse(static function ($value) {
+                            return $value > 0;
+                        })
+                        ->toAverage();
+                },
+                null,
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterFalse(static function ($value) {
+                            return $value > 0;
+                        })
+                        ->toAverage();
+                },
+                -2,
             ],
             [
                 [],
---
+++ tests/Chain/SingleTest.php
@@ -0,0 +1,271 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IterTools\Tests\Chain;
+
+use IterTools\Chain;
+
+class SingleTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @param array $input
+     * @param callable $chainMaker
+     * @param array $expected
+     * @return void
+     * @dataProvider dataProviderForArray
+     */
+    public function testArray(array $input, callable $chainMaker, array $expected): void
+    {
+        // Given
+        $result = [];
+
+        // When
+        $chain = $chainMaker($input);
+        foreach ($chain as $key => $value) {
+            $result[$key] = $value;
+        }
+
+        // Then
+        $this->assertSame($expected, $result);
+    }
+
+    public function dataProviderForArray(): array
+    {
+        return [
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable);
+                },
+                [],
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable);
+                },
+                [1, -1, 2, -2, 3, -3],
+            ],
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterTrue(static function ($value) {
+                            return $value > 0;
+                        });
+                },
+                [],
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterTrue(static function ($value) {
+                            return $value > 0;
+                        });
+                },
+                [1, 2, 3],
+            ],
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->takeWhile(static function ($value) {
+                            return abs($value) < 3;
+                        });
+                },
+                [],
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->takeWhile(static function ($value) {
+                            return abs($value) < 3;
+                        });
+                },
+                [1, -1, 2, -2],
+            ],
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->takeWhile(static function ($value) {
+                            return abs($value) < 3;
+                        })
+                        ->compress([0, 1, 0, 1]);
+                },
+                [],
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->takeWhile(static function ($value) {
+                            return abs($value) < 3;
+                        })
+                        ->compress([0, 1, 0, 1]);
+                },
+                [-1, -2],
+            ],
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->dropWhile(static function ($value) {
+                            return abs($value) < 3;
+                        })
+                        ->compress([0, 1]);
+                },
+                [],
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->dropWhile(static function ($value) {
+                            return abs($value) < 3;
+                        })
+                        ->compress([0, 1]);
+                },
+                [-3],
+            ],
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterTrue(static function ($value) {
+                            return $value > 0;
+                        })
+                        ->compress([0, 1, 1]);
+                },
+                [],
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterTrue(static function ($value) {
+                            return $value > 0;
+                        })
+                        ->compress([0, 1, 1]);
+                },
+                [2, 3],
+            ],
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterTrue(static function ($value) {
+                            return $value > 0;
+                        })
+                        ->compress([0, 1, 1]);
+                },
+                [],
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterTrue(static function ($value) {
+                            return $value > 0;
+                        })
+                        ->compress([0, 1, 1]);
+                },
+                [2, 3],
+            ],
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterFalse(static function ($value) {
+                            return $value > 0;
+                        })
+                        ->pairwise();
+                },
+                [],
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterFalse(static function ($value) {
+                            return $value > 0;
+                        });
+                },
+                [-1, -2, -3],
+            ],
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterTrue(static function ($value) {
+                            return $value > 0;
+                        })
+                        ->pairwise();
+                },
+                [],
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterTrue(static function ($value) {
+                            return $value > 0;
+                        })
+                        ->pairwise();
+                },
+                [[1, 2], [2, 3]],
+            ],
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterFalse(static function ($value) {
+                            return $value > 0;
+                        })
+                        ->pairwise();
+                },
+                [],
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterFalse(static function ($value) {
+                            return $value > 0;
+                        })
+                        ->pairwise();
+                },
+                [[-1, -2], [-2, -3]],
+            ],
+            [
+                [],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterFalse(static function ($value) {
+                            return $value % 2 === 0;
+                        })
+                        ->groupBy(static function($item) {
+                            return $item > 0 ? 'pos' : 'neg';
+                        });
+                },
+                [],
+            ],
+            [
+                [1, -1, 2, -2, 3, -3],
+                static function (array $iterable) {
+                    return Chain::create($iterable)
+                        ->filterFalse(static function ($value) {
+                            return $value % 2 === 0;
+                        })
+                        ->groupBy(static function($item) {
+                            return $item > 0 ? 'pos' : 'neg';
+                        });
+                },
+                ['pos' => [1, 3], 'neg' => [-1, -3]],
+            ],
+        ];
+    }
+}
