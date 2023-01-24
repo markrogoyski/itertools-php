83fe4b4324da78e133916a44911acb43cd064e99
Smoren <ofigate@gmail.com>
2023-01-24 22:52:27 +0300

    PHPdoc added for `FileFixture`.

--- tests/Fixture/FileFixture.php
+++ tests/Fixture/FileFixture.php
@@ -4,11 +4,19 @@ namespace IterTools\Tests\Fixture;
 
 class FileFixture
 {
+    /**
+     * @param array<string> $lines
+     * @return resource
+     */
     public static function createFromLines(array $lines)
     {
         return self::createFromString(implode("\n", $lines));
     }
 
+    /**
+     * @param string $string
+     * @return resource
+     */
     public static function createFromString(string $string)
     {
         $file = tmpfile();
