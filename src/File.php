68b6abe9ae10194c056cb1a90fcef1bef8f840f9
Smoren <ofigate@gmail.com>
2023-01-24 11:38:18 +0300

    `File` namespace: applying initial suggestions.

--- src/File.php
+++ src/File.php
@@ -10,16 +10,15 @@ class File
      * Reads file resource line by line.
      *
      * @param resource $fileResource
-     * @param int<0, max>|null $length
      *
      * @return \Generator<string>
      *
      * @see fgets()
      */
-    public static function readByLine($fileResource, ?int $length = null): \Generator
+    public static function readByLine($fileResource): \Generator
     {
         // @phpstan-ignore-next-line (expects int<0, max>, int<0, max>|null given.)
-        while (($line = @\fgets($fileResource, $length)) !== false) {
+        while (($line = @\fgets($fileResource, null)) !== false) {
             yield $line;
         }
     }
@@ -28,7 +27,6 @@ class File
      * Reads data from CSV file resource like fgetcsv() function.
      *
      * @param resource $fileResource
-     * @param int<0, max>|null $length
      * @param string $separator
      * @param string $enclosure
      * @param string $escape
@@ -41,61 +39,17 @@ class File
      */
     public static function readCsv(
         $fileResource,
-        ?int $length = null,
         string $separator = ",",
         string $enclosure = "\"",
         string $escape = "\\"
     ): \Generator {
         // @phpstan-ignore-next-line (expects int<0, max>, int<0, max>|null given.)
-        while (($row = @\fgetcsv($fileResource, $length, $separator, $enclosure, $escape)) !== false) {
+        while (($row = @\fgetcsv($fileResource, null, $separator, $enclosure, $escape)) !== false) {
             if ($row === null) {
                 throw new \RuntimeException('cannot read from file resource');
             }
 
             yield $row;
         }
-    }
-
-    /**
-     * Writes data to the file resource.
-     *
-     * Data items must be stringifiable.
-     *
-     * Returns count of written bytes.
-     *
-     * @param resource $fileResource
-     * @param iterable<mixed> $data
-     * @param string $suffix
-     * @param string $prefix
-     * @param int<0, max>|null $length
-     *
-     * @return int
-     *
-     * @throws \RuntimeException if write error occurred
-     *
-     * @see fputs()
-     */
-    public static function write(
-        $fileResource,
-        iterable $data,
-        string $suffix = '',
-        string $prefix = '',
-        ?int $length = null
-    ): int {
-        $bytesWrote = 0;
-
-        /** @var string $datum */
-        foreach ($data as $datum) {
-            // @phpstan-ignore-next-line (expects int<0, max>, int<0, max>|null given.)
-            $writeResult = @\fputs($fileResource, "{$prefix}{$datum}{$suffix}", $length);
-
-            if ($writeResult === false) {
-                throw new \RuntimeException('cannot write to file resource');
-            }
-
-            $bytesWrote += $writeResult;
-        }
-
-        return $bytesWrote;
     }
 }
