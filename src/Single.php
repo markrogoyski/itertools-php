4a32722b61143ce4a45998d1c0b7eda7852178fb
Smoren <ofigate@gmail.com>
2023-02-08 10:17:29 +0300

    `Single::sort()` method removed.

--- src/Single.php
+++ src/Single.php
@@ -367,31 +367,6 @@ class Single
     }
 
     /**
-     * Sorts the given iterable
-     *
-     * If comparator is null, the elements of given iterable must be comparable.
-     *
-     * @param iterable<mixed> $data
-     * @param callable|null $comparator (optional) function to determine how to sort elements if default sort is not appropriate.
-     *
-     * @return \Generator
-     */
-    public static function sort(iterable $data, callable $comparator = null): \Generator
-    {
-        $result = \iterator_to_array(IteratorFactory::makeIterator($data));
-
-        if ($comparator === null) {
-            \sort($result);
-        } else {
-            \usort($result, $comparator);
-        }
-
-        foreach ($result as $datum) {
-            yield $datum;
-        }
-    }
-
-    /**
      * Reverse given iterable.
      *
      * @param iterable<mixed> $data
